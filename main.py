0ce6a932f7846e5778d20a0349f57f8f01e7b1e6
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-11-27 17:43:59 +0100

    Version_nouveau

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ import pickle
 
 model = pickle.load(open('model.pkl', 'rb'))
 
-# st.write("L'application qui prédit l'accord du crédit")
+st.write("L'application qui prédit l'accord du crédit")
+model = pickle.load(open('model.pkl', 'rb'))
+
 
 #Collecter le profil d'entrée
 st.sidebar.header("Les caracteristiques du client")
 
-def run():
-    img1 = Image.open('image4.jpg')
-    img1 = img1.resize((180, 180))
-    st.image(img1, use_column_width=False)
-    
-    
+def client_caract_entree():
     Gender=st.sidebar.selectbox('Sexe',('Male','Female'))
     Married=st.sidebar.selectbox('Marié',('Yes','No'))
     Dependents=st.sidebar.selectbox('Enfants',('0','1','2','3+'))
@@ -27,93 +24,169 @@ def run():
     Credit_History=st.sidebar.selectbox('Credit_History',(1.0,0.0))
     Property_Area=st.sidebar.selectbox('Property_Area',('Urban','Rural','Semiurban'))
     
+
+    data={
+    'Gender':Gender,
+    'Married':Married,
+    'Dependents':Dependents,
+    'Education':Education,
+    'Self_Employed':Self_Employed,
+    'ApplicantIncome':ApplicantIncome,
+    'CoapplicantIncome':CoapplicantIncome,
+    'LoanAmount':LoanAmount,
+    'Loan_Amount_Term':Loan_Amount_Term,
+    'Credit_History':Credit_History,
+    'Property_Area':Property_Area
+    }
+
+    profil_client=pd.DataFrame(data,index=[0])
+    return profil_client
+
+input_df=client_caract_entree()
+
+#Transformer les données d'entrée en données adaptées à notre modèle
+#importer la base de données
+df=pd.read_csv('train.csv')
+credit_input=df.drop(columns=['Loan_ID','Loan_Status'])
+donnee_entree=pd.concat([input_df,credit_input],axis=0)
+
+# encodage des données
+var_cat=['Gender', 'Married', 'Dependents', 'Education','Self_Employed','Credit_History', 'Property_Area']
+for col in var_cat:
+    # dummy=pd.get_dummies(donnee_entree[col],drop_first=True)
+    # donnee_entree=pd.concat([dummy,donnee_entree],axis=1)
+    del donnee_entree[col]
+#prendre uniquement la premiere ligne
+donnee_entree=donnee_entree[:1]
+
+#afficher les données transformées
+st.subheader('Les caracteristiques transformés')
+st.write(donnee_entree)
+
+
+#importer le modèle
+load_model=pickle.load(open('model.pkl','rb'))
+
+
+#appliquer le modèle sur le profil d'entrée
+prevision=load_model.predict(donnee_entree)
+
+st.subheader('Résultat de la prévision')
+# st.write(prevision)
+
+
+
+
+# st.write("L'application qui prédit l'accord du crédit")
+
+#Collecter le profil d'entrée
+# st.sidebar.header("Les caracteristiques du client")
+
+# def run():
+#     img1 = Image.open('image4.jpg')
+#     img1 = img1.resize((180, 180))
+#     st.image(img1, use_column_width=False)
+    
+    
+#     Gender=st.sidebar.selectbox('Sexe',('Male','Female'))
+#     Married=st.sidebar.selectbox('Marié',('Yes','No'))
+#     Dependents=st.sidebar.selectbox('Enfants',('0','1','2','3+'))
+#     Education=st.sidebar.selectbox('Education',('Graduate','Not Graduate'))
+#     Self_Employed=st.sidebar.selectbox('Salarié ou Entrepreneur',('Yes','No'))
+#     ApplicantIncome=st.sidebar.slider('Salaire du client',150,4000,200)
+#     CoapplicantIncome=st.sidebar.slider('Salaire du conjoint',0,40000,2000)
+#     LoanAmount=st.sidebar.slider('Montant du crédit en Kdollar',9.0,700.0,200.0)
+#     Loan_Amount_Term=st.sidebar.selectbox('Durée du crédit',(360.0,120.0,240.0,180.0,60.0,300.0,36.0,84.0,12.0))
+#     Credit_History=st.sidebar.selectbox('Credit_History',(1.0,0.0))
+#     Property_Area=st.sidebar.selectbox('Property_Area',('Urban','Rural','Semiurban'))
+    
     
 
 
-    new_title = '<p style="font-family:sans-serif; color:Red; font-size: 20px;">LOAN PREDICTION</p>'
-    st.markdown(new_title, unsafe_allow_html=True)
-    title = '<p style="font-family:sans-serif; color:Blue; font-size: 30px;">Bank Simplonien</p>'
+#     new_title = '<p style="font-family:sans-serif; color:Red; font-size: 20px;">LOAN PREDICTION</p>'
+#     st.markdown(new_title, unsafe_allow_html=True)
+#     title = '<p style="font-family:sans-serif; color:Blue; font-size: 30px;">Bank Simplonien</p>'
 
 
-    st.markdown(title,unsafe_allow_html=True)
+#     st.markdown(title,unsafe_allow_html=True)
 
-    fn = st.text_input('Full Name')
-    account_no = st.text_input('Account number')
-
-
+#     fn = st.text_input('Full Name')
+#     account_no = st.text_input('Account number')
 
 
-    gen_display = ('Female','Male')
-    gen_options = list(range(len(gen_display)))
-    gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
+
+
+#     gen_display = ('Female','Male')
+#     gen_options = list(range(len(gen_display)))
+#     gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
 
    
-    edu_display = ('Not Graduate', 'Graduate')
-    edu_options = list(range(len(edu_display)))
-    edu = st.selectbox("Education", edu_options, format_func=lambda x: edu_display[x])
+#     edu_display = ('Not Graduate', 'Graduate')
+#     edu_options = list(range(len(edu_display)))
+#     edu = st.selectbox("Education", edu_options, format_func=lambda x: edu_display[x])
     
-    mar_display = ('No', 'Yes')
-    mar_options = list(range(len(mar_display)))
-    mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
-
-    
-    dep_display = ('No', 'One', 'Two', 'More than Two')
-    dep_options = list(range(len(dep_display)))
-    dep = st.selectbox("Dependents", dep_options, format_func=lambda x: dep_display[x])
-
-
+#     mar_display = ('No', 'Yes')
+#     mar_options = list(range(len(mar_display)))
+#     mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
 
     
-    emp_display = ('Job', 'Business')
-    emp_options = list(range(len(emp_display)))
-    emp = st.selectbox("Employment Status", emp_options, format_func=lambda x: emp_display[x])
+#     dep_display = ('No', 'One', 'Two', 'More than Two')
+#     dep_options = list(range(len(dep_display)))
+#     dep = st.selectbox("Dependents", dep_options, format_func=lambda x: dep_display[x])
+
+
 
     
-    prop_display = ('Rural', 'Semi-Urban', 'Urban')
-    prop_options = list(range(len(prop_display)))
-    prop = st.selectbox("Property Area", prop_options, format_func=lambda x: prop_display[x])
+#     emp_display = ('Job', 'Business')
+#     emp_options = list(range(len(emp_display)))
+#     emp = st.selectbox("Employment Status", emp_options, format_func=lambda x: emp_display[x])
 
     
-    cred_display = ('Between 300 to 500', 'Above 500')
-    cred_options = list(range(len(cred_display)))
-    cred = st.selectbox("Credit Score", cred_options, format_func=lambda x: cred_display[x])
-    
-    mon_income = st.number_input("Applicant's Monthly Income($)", value=0)
+#     prop_display = ('Rural', 'Semi-Urban', 'Urban')
+#     prop_options = list(range(len(prop_display)))
+#     prop = st.selectbox("Property Area", prop_options, format_func=lambda x: prop_display[x])
 
     
-    co_mon_income = st.number_input("Co-Applicant's Monthly Income($)", value=0)
+#     cred_display = ('Between 300 to 500', 'Above 500')
+#     cred_options = list(range(len(cred_display)))
+#     cred = st.selectbox("Credit Score", cred_options, format_func=lambda x: cred_display[x])
+    
+#     mon_income = st.number_input("Applicant's Monthly Income($)", value=0)
 
     
-    loan_amt = st.number_input("Loan Amount", value=0)
+#     co_mon_income = st.number_input("Co-Applicant's Monthly Income($)", value=0)
 
     
-    dur_display = ['2 Month', '6 Month', '8 Month', '1 Year', '16 Month']
-    dur_options = range(len(dur_display))
-    dur = st.selectbox("Loan Duration", dur_options, format_func=lambda x: dur_display[x])
-    if st.button("Submit"):
-        duration = 0
-        if dur == 0:
-            duration = 60
-        if dur == 1:
-            duration = 180
-        if dur == 2:
-            duration = 240
-        if dur == 3:
-            duration = 360
-        if dur == 4:
-            duration = 480
-        features = [[gen, mar, dep, edu, emp, mon_income, co_mon_income, loan_amt, duration, cred, prop]]
-        print(features)
-        prediction = model.predict(features)
-        lc = [str(i) for i in prediction]
-        ans = int("".join(lc))
-        if ans == 0:
-            st.error(
-                "Hello " + fn +' you will not get a loan as per the calculations of the bank.'
-            )
-        else:
-            st.success(
-                "Hello " + fn + ' '+' Congratulations!! you will get the loan from Bank'
-            )
+#     loan_amt = st.number_input("Loan Amount", value=0)
 
-run()
+    
+#     dur_display = ['2 Month', '6 Month', '8 Month', '1 Year', '16 Month']
+#     dur_options = range(len(dur_display))
+#     dur = st.selectbox("Loan Duration", dur_options, format_func=lambda x: dur_display[x])
+#     if st.button("Submit"):
+#         duration = 0
+#         if dur == 0:
+#             duration = 60
+#         if dur == 1:
+#             duration = 180
+#         if dur == 2:
+#             duration = 240
+#         if dur == 3:
+#             duration = 360
+#         if dur == 4:
+#             duration = 480
+#         features = [[gen, mar, dep, edu, emp, mon_income, co_mon_income, loan_amt, duration, cred, prop]]
+#         print(features)
+#         prediction = model.predict(features)
+#         lc = [str(i) for i in prediction]
+#         ans = int("".join(lc))
+#         if ans == 0:
+#             st.error(
+#                 "Hello " + fn +' you will not get a loan as per the calculations of the bank.'
+#             )
+#         else:
+#             st.success(
+#                 "Hello " + fn + ' '+' Congratulations!! you will get the loan from Bank'
+#             )
+
+# run()
