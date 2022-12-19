bd3cf7062dfda0bcb984d2c16ef3c17875917423
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-19 11:49:00 +0100

    New_App.main.py

--- main.py
+++ main.py
@@ -24,52 +24,52 @@ def client_caract_entree():
     Credit_History=st.sidebar.selectbox('Credit_History',(1.0,0.0))
     Property_Area=st.sidebar.selectbox('Property_Area',('Urban','Rural','Semiurban'))
 
-    data={
-    'Gender':Gender,
-    'Married':Married,
-    'Dependents':Dependents,
-    'Education':Education,
-    'Self_Employed':Self_Employed,
-    'ApplicantIncome':ApplicantIncome,
-    'CoapplicantIncome':CoapplicantIncome,
-    'LoanAmount':LoanAmount,
-    'Loan_Amount_Term':Loan_Amount_Term,
-    'Credit_History':Credit_History,
-    'Property_Area':Property_Area
-    }
+#     data={
+#     'Gender':Gender,
+#     'Married':Married,
+#     'Dependents':Dependents,
+#     'Education':Education,
+#     'Self_Employed':Self_Employed,
+#     'ApplicantIncome':ApplicantIncome,
+#     'CoapplicantIncome':CoapplicantIncome,
+#     'LoanAmount':LoanAmount,
+#     'Loan_Amount_Term':Loan_Amount_Term,
+#     'Credit_History':Credit_History,
+#     'Property_Area':Property_Area
+#     }
 
-    profil_client=pd.DataFrame(data,index=[0])
-    return profil_client
+#     profil_client=pd.DataFrame(data,index=[0])
+#     return profil_client
 
-input_df=client_caract_entree()
+# input_df=client_caract_entree()
 
-#Transformer les données d'entrée en données adaptées à notre modèle
-#importer la base de données
-df=pd.read_csv('train.csv')
-credit_input=df.drop(columns=['Loan_ID','Loan_Status'])
-donnee_entree=pd.concat([input_df,credit_input],axis=0)
+# #Transformer les données d'entrée en données adaptées à notre modèle
+# #importer la base de données
+# df=pd.read_csv('train.csv')
+# credit_input=df.drop(columns=['Loan_ID','Loan_Status'])
+# donnee_entree=pd.concat([input_df,credit_input],axis=0)
 
-# encodage des données
-var_cat=['Gender', 'Married', 'Dependents', 'Education','Self_Employed','Credit_History', 'Property_Area']
-for col in var_cat:
-    dummy=pd.get_dummies(donnee_entree[col],drop_first=True)
-    donnee_entree=pd.concat([dummy,donnee_entree],axis=1)
-    del donnee_entree[col]
-#prendre uniquement la premiere ligne
-donnee_entree=donnee_entree[:1]
+# # encodage des données
+# var_cat=['Gender', 'Married', 'Dependents', 'Education','Self_Employed','Credit_History', 'Property_Area']
+# for col in var_cat:
+#     dummy=pd.get_dummies(donnee_entree[col],drop_first=True)
+#     donnee_entree=pd.concat([dummy,donnee_entree],axis=1)
+#     del donnee_entree[col]
+# #prendre uniquement la premiere ligne
+# donnee_entree=donnee_entree[:1]
 
-#afficher les données transformées
-st.subheader('Les caracteristiques transformés')
-st.write(donnee_entree)
-
-
-#importer le modèle
-load_model=pickle.load(open('prevision_credit.pkl','rb'))
+# #afficher les données transformées
+# st.subheader('Les caracteristiques transformés')
+# st.write(donnee_entree)
 
 
-#appliquer le modèle sur le profil d'entrée
-prevision=load_model.predict(donnee_entree)
+# #importer le modèle
+# load_model=pickle.load(open('prevision_credit.pkl','rb'))
 
-st.subheader('Résultat de la prévision')
-st.write(prevision)
+
+# #appliquer le modèle sur le profil d'entrée
+# prevision=load_model.predict(donnee_entree)
+
+# st.subheader('Résultat de la prévision')
+# st.write(prevision)
             
