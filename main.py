87aba98b132888bcced9af421a0da39bf4e8617c
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-19 21:32:51 +0100

    New_version3.main.py

--- main.py
+++ main.py
@@ -82,44 +82,44 @@ prevision=load_model.predict(donnee_entree)
 st.subheader('Résultat de la prévision')
 st.write(prevision)
 
-    gen_display = ('Female','Male')
-    gen_options = list(range(len(gen_display)))
-    gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
+gen_display = ('Female','Male')
+gen_options = list(range(len(gen_display)))
+gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
 
-    edu_display = ('Not Graduate', 'Graduate')
-    edu_options = list(range(len(edu_display)))
-    edu = st.selectbox("Education", edu_options, format_func=lambda x: edu_display[x])
+edu_display = ('Not Graduate', 'Graduate')
+edu_options = list(range(len(edu_display)))
+edu = st.selectbox("Education", edu_options, format_func=lambda x: edu_display[x])
     
-    mar_display = ('No', 'Yes')
-    mar_options = list(range(len(mar_display)))
-    mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
+mar_display = ('No', 'Yes')
+mar_options = list(range(len(mar_display)))
+mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
 
-    dep_display = ('No', 'One', 'Two', 'More than Two')
-    dep_options = list(range(len(dep_display)))
-    dep = st.selectbox("Dependents", dep_options, format_func=lambda x: dep_display[x])
+dep_display = ('No', 'One', 'Two', 'More than Two')
+dep_options = list(range(len(dep_display)))
+dep = st.selectbox("Dependents", dep_options, format_func=lambda x: dep_display[x])
     
-    emp_display = ('Job', 'Business')
-    emp_options = list(range(len(emp_display)))
-    emp = st.selectbox("Employment Status", emp_options, format_func=lambda x: emp_display[x])
+emp_display = ('Job', 'Business')
+emp_options = list(range(len(emp_display)))
+emp = st.selectbox("Employment Status", emp_options, format_func=lambda x: emp_display[x])
 
-    prop_display = ('Rural', 'Semi-Urban', 'Urban')
-    prop_options = list(range(len(prop_display)))
-    prop = st.selectbox("Property Area", prop_options, format_func=lambda x: prop_display[x])
+prop_display = ('Rural', 'Semi-Urban', 'Urban')
+prop_options = list(range(len(prop_display)))
+prop = st.selectbox("Property Area", prop_options, format_func=lambda x: prop_display[x])
     
-    cred_display = ('Between 300 to 500', 'Above 500')
-    cred_options = list(range(len(cred_display)))
-    cred = st.selectbox("Credit Score", cred_options, format_func=lambda x: cred_display[x])
+cred_display = ('Between 300 to 500', 'Above 500')
+cred_options = list(range(len(cred_display)))
+cred = st.selectbox("Credit Score", cred_options, format_func=lambda x: cred_display[x])
     
-    mon_income = st.number_input("Applicant's Monthly Income($)", value=0)
+mon_income = st.number_input("Applicant's Monthly Income($)", value=0)
 
-    co_mon_income = st.number_input("Co-Applicant's Monthly Income($)", value=0)
+co_mon_income = st.number_input("Co-Applicant's Monthly Income($)", value=0)
 
-    loan_amt = st.number_input("Loan Amount", value=0)
+loan_amt = st.number_input("Loan Amount", value=0)
 
-    dur_display = ['2 Month', '6 Month', '8 Month', '1 Year', '16 Month']
-    dur_options = range(len(dur_display))
-    dur = st.selectbox("Loan Duration", dur_options, format_func=lambda x: dur_display[x])
-    if st.button("Submit"):
+dur_display = ['2 Month', '6 Month', '8 Month', '1 Year', '16 Month']
+dur_options = range(len(dur_display))
+dur = st.selectbox("Loan Duration", dur_options, format_func=lambda x: dur_display[x])
+if st.button("Submit"):
         duration = 0
         if dur == 0:
             duration = 60
