00259350caeddcceffd9bd844179dcd4b403d4be
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-19 20:59:27 +0100

    New_version2.main.py

--- main.py
+++ main.py
@@ -20,7 +20,34 @@ st.markdown(new_title, unsafe_allow_html=True)
 
 # fn = st.text_input('Full Name')
 # account_no = st.text_input('Account number')
+def client_caract_entree():
+ Gender=st.sidebar.selectbox('Sexe',('Male','Female'))
+ Married=st.sidebar.selectbox('Marié',('Yes','No'))
+ Dependents=st.sidebar.selectbox('Enfants',('0','1','2','3+'))
+ Education=st.sidebar.selectbox('Education',('Graduate','Not Graduate'))
+ Self_Employed=st.sidebar.selectbox('Salarié ou Entrepreneur',('Yes','No'))
+ ApplicantIncome=st.sidebar.slider('Salaire du client',150,4000,200)
+ CoapplicantIncome=st.sidebar.slider('Salaire du conjoint',0,40000,2000)
+ LoanAmount=st.sidebar.slider('Montant du crédit en Kdollar',9.0,700.0,200.0)
+ Loan_Amount_Term=st.sidebar.selectbox('Durée du crédit',(360.0,120.0,240.0,180.0,60.0,300.0,36.0,84.0,12.0))
+ Credit_History=st.sidebar.selectbox('Credit_History',(1.0,0.0))
+ Property_Area=st.sidebar.selectbox('Property_Area',('Urban','Rural','Semiurban'))
 
+
+ data={
+ 'Gender':Gender,
+ 'Married':Married,
+ 'Dependents':Dependents,
+ 'Education':Education,
+ 'Self_Employed':Self_Employed,
+ 'ApplicantIncome':ApplicantIncome,
+ 'CoapplicantIncome':CoapplicantIncome,
+ 'LoanAmount':LoanAmount,
+ 'Loan_Amount_Term':Loan_Amount_Term,
+ 'Credit_History':Credit_History,
+ 'Property_Area':Property_Area
+ }
+ 
 gen_display = ('Female','Male')
 gen_options = list(range(len(gen_display)))
 gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
