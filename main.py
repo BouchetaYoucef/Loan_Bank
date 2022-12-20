13962e7fd583347a29e13a8f5860ba5b91e3538f
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-20 10:15:32 +0100

    New_version3.main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def run():
     st.sidebar.header("L'application qui prédit l'accord du crédit")
 
     # new_title = '<p style="font-family:sans-serif; color:red; font-size: 20px;">BANK SIMPLONIEN</p>'
-    st.markdown(new_title, unsafe_allow_html=True)
+    # st.markdown(new_title, unsafe_allow_html=True)
     # title = '<p style="font-family:sans-serif; color:orange; font-size: 30px;">B SIMPLONIEN</p>'
 
     gen_display = ('Female','Male')
