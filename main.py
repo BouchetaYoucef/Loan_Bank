2a13e91f89e84cd2be8e650120dfc1e9459acad4
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-20 10:15:03 +0100

    New_version3.main.py

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ def run():
 
     st.sidebar.header("L'application qui prédit l'accord du crédit")
 
-    new_title = '<p style="font-family:sans-serif; color:red; font-size: 20px;">BANK SIMPLONIEN</p>'
+    # new_title = '<p style="font-family:sans-serif; color:red; font-size: 20px;">BANK SIMPLONIEN</p>'
     st.markdown(new_title, unsafe_allow_html=True)
     # title = '<p style="font-family:sans-serif; color:orange; font-size: 30px;">B SIMPLONIEN</p>'
-
-    st.markdown(new_title,unsafe_allow_html=True)
-
 
     gen_display = ('Female','Male')
     gen_options = list(range(len(gen_display)))
@@ -45,7 +42,7 @@ def run():
     cred_options = list(range(len(cred_display)))
     cred = st.selectbox("Credit History", cred_options, format_func=lambda x: cred_display[x])
     
-    Titre=st.sidebar('Bank SIMPLONIEN',st.write("L'application qui prédit l'accord du crédit"))
+    # Titre=st.sidebar('BANK SIMPLONIEN',st.write("L'application qui prédit l'accord du crédit"))
     
     # cred_display = ('Between 300 to 500', 'Above 500')
     # cred_options = list(range(len(cred_display)))
