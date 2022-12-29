8bde986c7a961337a022094de6f54a4e92e67d9e
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-29 20:41:58 +0100

    new_app

--- Notebook/Static/main.py
+++ Notebook/Static/main.py
@@ -8,7 +8,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 st.write("L'application qui prédit l'accord du crédit")
 
 def run():
- img1 = Image.open('image4.jpg')
+#  img1 = Image.open('image4.jpg')
  img1 = img1.resize((180, 180))
  st.image(img1, use_column_width=False) 
  
@@ -61,8 +61,8 @@ donnee_entree=pd.concat([input_df,credit_input],axis=0)
 # encodage des données
 var_cat=['Gender', 'Married', 'Dependents', 'Education','Self_Employed','Credit_History', 'Property_Area']
 for col in var_cat:
- dummy=pd.get_dummies(donnee_entree[col],drop_first=True)
- donnee_entree=pd.concat([dummy,donnee_entree],axis=1)
+ lbe=pd.Labelencoder(donnee_entree[col],drop_first=True)
+ donnee_entree=pd.concat([lbe,donnee_entree],axis=1)
  del donnee_entree[col]
 #prendre uniquement la premiere ligne
 donnee_entree=donnee_entree[:1]
