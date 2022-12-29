57d7826ac89f5ea79243b28b36b34f60efb9b4dd
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2022-12-29 19:44:07 +0100

    new_app

--- Notebook/main.py
+++ Notebook/main.py
@@ -1,9 +1,12 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+import pandas as pd
+# from flask import Flask, request, jsonify, render_template
+import streamlit as st
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
@@ -20,18 +23,18 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='La réponse à votre reqête {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+# @app.route('/predict_api',methods=['POST'])
+# def predict_api():
+#     '''
+#     For direct API calls trought request
+#     '''
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
+#     output = prediction[0]
+#     return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
