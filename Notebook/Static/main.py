faeb7f24f60203c898ebae1aff2521f8b5a18167
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2023-01-15 17:23:11 +0100

    new_app

--- Notebook/Static/main.py
+++ Notebook/Static/main.py
@@ -13,36 +13,7 @@ run()
 
 def prediction(Gender, Married, Dependents, Employment_Status, ApplicantIncome, CoapplicantIncome,LoanAmount, Credit_History):
         
- Gender=st.sidebar.selectbox('Sexe',('Male','Female'))
- Married=st.sidebar.selectbox('Marié',('Yes','No'))
- Dependents=st.sidebar.selectbox('Enfants',('0','1','2','3+'))
- Education=st.sidebar.selectbox('Education',('Graduate','Not Graduate'))
- Self_Employed=st.sidebar.selectbox('Salarié ou Entrepreneur',('Yes','No'))
- ApplicantIncome=st.sidebar.slider('Salaire du client',150,4000,200)
- CoapplicantIncome=st.sidebar.slider('Salaire du conjoint',0,40000,2000)
- LoanAmount=st.sidebar.slider('Montant du crédit en Kdollar',9.0,700.0,200.0)
- Loan_Amount_Term=st.sidebar.selectbox('Durée du crédit',(360.0,120.0,240.0,180.0,60.0,300.0,36.0,84.0,12.0))
- Credit_History=st.sidebar.selectbox('Credit_History',(1.0,0.0))
- Property_Area=st.sidebar.selectbox('Property_Area',('Urban','Rural','Semiurban'))
- 
- data={
- 'Gender':Gender,
- 'Married':Married,
- 'Dependents':Dependents,
- 'Education':Education,
- 'Self_Employed':Self_Employed,
- 'ApplicantIncome':ApplicantIncome,
- 'CoapplicantIncome':CoapplicantIncome,
- 'LoanAmount':LoanAmount,
- 'Loan_Amount_Term':Loan_Amount_Term,
-'Credit_History':Credit_History,
- 'Property_Area':Property_Area
- }
-
-profil_client=pd.DataFrame(data,index=[0])
- return profil_client
-
-    input_df=client_caract_entree()
+    v
     # if st.button("Submit"):
     
     # Making predictions
@@ -56,9 +27,9 @@ profil_client=pd.DataFrame(data,index=[0])
     return pred
 
 # this is the main function in which we define our webpage
-    def main():
+def main():
     # front end elements of the web page
-     html_temp = """
+    html_temp = """
     <div style ="background-color:blue;padding:13px">
     <h1 style ="color:white;text-align:center;">Bank Simplonien</h1>
     </div>
@@ -83,5 +54,5 @@ profil_client=pd.DataFrame(data,index=[0])
         st.success('Your loan is {}'.format(result))
         print(LoanAmount)
         
-    if __name__=='__main__':
-     main()
+if __name__=='__main__':
+    main()
